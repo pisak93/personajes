186c72c130b08912403454950632ec9ba34b15f1
pisak93 <sebastian.marquez93@hotmail.com>
2026-05-08 01:28:36 -0500

    Actualización de URL con render

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,9 @@ import Card from "../assets/componentes/Card";
 
 export default function Index() {
 
-  const URL_API ="http://192.168.40.15:5000";
+ // const URL_API ="http://192.168.40.15:5000";
+ const URL_API =" https://backend-personajes-n9m9.onrender.com"
+
 
   const [personajes, setPersonajes]= useState([]);
   const [filtro, setFiltro]= useState({});
