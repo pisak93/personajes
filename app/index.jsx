81a06f44711acc42616e6a592b29482e35faf984
pisak93 <sebastian.marquez93@hotmail.com>
2026-05-08 01:21:49 -0500

    Se agregaron funciones de delete y crear

--- app/index.jsx
+++ app/index.jsx
@@ -1,55 +1,177 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { FlatList } from "react-native-web";
+import { FlatList, Pressable, Text, TextInput, View } from "react-native";
+import global from "../assets/Styles/Stylesheet";
 import Card from "../assets/componentes/Card";
 
 export default function Index() {
 
-  const [personajes, setPersonajes] = useState([]);
+  const [personajes, setPersonajes]= useState([]);
+  const [filtro, setFiltro]= useState({});
+  const [nuevoPersonaje, setNuevoPersonaje]= useState({});
 
-function getPersonajes() {
-  console.log("Obteniendo personajes...");
+  function actualizarFiltro(key,value){
+setFiltro({
+  ...filtro,
+  [key]:value
+})
+  }
 
-  const response = axios.get("http://192.168.40.15:5000/api/v1/personajes")
-  .then(function(res){
-   const data = res.data.data;
-    setPersonajes(data);
-  })
-  .catch(function(err){
-    console.log(err);
-  })
-}
+  function getPersonajes(){
 
-useEffect(function(){
-  getPersonajes();
-}, []);
+    const response = axios.get("http://192.168.40.15:5000/api/v1/personajes/filtro",
+     { params: filtro
+    }
+    )
+    .then(function(res){
+      const datos = res.data.data;
+      setPersonajes(datos);
 
-function itemCard({item}){
-  return(
-    <Card
-    nombre={item.nombre}
-    clase={item.clase}
-    xp={item.experiencia}
-    hp={item.healthPoints}
-    mana={item.mana}
-    fuerza={item.fuerza}
-    velocidad={item.velocidad}
-    habilidades={item.habilidades}
-    />
-  )
+    })
+    .catch(function(error){
+      console.log(error);
+      alert(error);
+    })
 
-}
+
+  }
+
+    function crearPersonaje(){
+    const response = axios.post("http://192.168.40.15:5000/api/v1/personajes",
+      {
+             nombre:"Pedro",
+        clase:"Mago",
+         experiencia:1800 ,
+  healthPoints:200,
+  mana:520,
+  velocidad:10,
+  fuerza:50,
+  habildades:["Lanzallamas, Bomba de humo, Loro"]
+      }
+    )
+    .then(function(){
+       getPersonajes();
+      alert("Personaje creado Correctamente");
+    })
+    .catch(function(error){
+console.log(error.response.data
+)
+    })
+  }
+
+    function eliminarPersonaje(id){
+        const response = axios.delete("http://192.168.40.15:5000/api/v1/personajes/"+id)
+        .then(function(){
+             alert("Personaje eliminado Correctamente");
+              setPersonajes(
+      personajes.filter((p) => p._id !== id)
+    );
+        })
+        .catch(function(error){
+console.log(error.response.data.message
+)
+        })
+    }
+
+
+  useEffect(function(){
+  
+    const timeout = setTimeout(function(){
+ getPersonajes();
+    },500)
+
+
+  return function(){clearTimeout(timeout);}
+   
+  },[filtro]);
+
+
+  function itemCard({item}){
+    
+
+    return(
+      <Card
+      id={item._id}
+      nombre={item.nombre}
+      clase={item.clase}
+      xp={item.experiencia}
+      hp={item.healthPoints}
+      mana={item.mana}
+      fuerza={item.fuerza}
+      velocidad={item.velocidad}
+      habilidades={item.habilidades}
+      eliminar={eliminarPersonaje}
+      />
+    )
+
+  }
+
+
+
 
 
   return (
-    <View>
-      <Text>Personajes</Text>
+    <View style={global.contenedor}>
+      <View style={global.contenedor} >
+   <Text style={global.titulo}>Personajes</Text>
       <FlatList
       data={personajes}
-      contentContainerStyle={{flexWrap: "wrap", flexDirection: "row", justifyContent: "center", gap: 20}}
       renderItem={itemCard}
+      numColumns={3}
+      contentContainerStyle={global.flatCont}
+      columnWrapperStyle={global.rowCont}
       />
+      </View>
+   <View>
+     <Text>Nombre</Text>
+    <TextInput
+    placeholder="Ej:Rynn"
+    value={filtro.nombre}
+    onChangeText={function(text){actualizarFiltro("nombre",text)}}
+
+    />
+    <Text>Clase</Text>
+    <TextInput
+    placeholder="Ej: Guerrero"
+    value={filtro.clase}
+    onChangeText={function(text){actualizarFiltro("clase",text)}}
+
+    />
+
+     <Text>Experiencia</Text>
+     <View>
+      <Text>Min</Text>
+  <TextInput
+    placeholder="0"
+    value={filtro.expMin}
+    onChangeText={function(text){actualizarFiltro("expMin",text)}}
+
+    />
+
+        <Text>Max</Text>
+  <TextInput
+    placeholder="5000"
+    value={filtro.expMax}
+    onChangeText={function(text){actualizarFiltro("expMax",text)}}
+
+    />
+
+     </View>
+  
+  <View>
+    <Text>Crear Personaje</Text>
+    <Pressable onPress={crearPersonaje} ><Text>Crear</Text></Pressable>
+
+  </View>
+
+
+
+
+
+   </View>
+
+   
+    
     </View>
   );
 }
--- assets/componentes/Card.jsx
+++ assets/componentes/Card.jsx
@@ -1,31 +1,36 @@
+import { Pressable } from "react-native";
 import { Text, View } from "react-native-web";
+import global from "../Styles/Stylesheet";
 
 
-function Card({ nombre, clase, xp, hp, mana, fuerza, velocidad, habilidades }) {
+function Card({ nombre, clase, xp, hp, mana, fuerza, velocidad, habilidades,id,eliminar }) {
+ 
 
+  
     return(
-        <View>
-            <Text>{nombre}</Text>
-            <Text>{clase}</Text>
-            <View>
-                <Text>XP: {xp}</Text>
-                <Text>HP: {hp}</Text>
-                <Text>Mana: {mana}</Text>
+        <View id={id} style={[global.carta, global[clase]]}>
+            <Text style={global.nombre}>{nombre}</Text>
+            <Text style={global.clase}>{clase}</Text>
+            <View style={global.statsCont}>
+                <Text style={global.statsText}>XP: <Text style={global.statsDato}>{xp}</Text></Text>
+                <Text style={global.statsText}>HP: <Text style={global.statsDato}>{hp}</Text></Text>
+                <Text style={global.statsText}>Mana: <Text style={global.statsDato}>{mana}</Text></Text>
             </View>
-            <View>
-                <Text>Fuerza: {fuerza}</Text>
-                <Text>Velocidad: {velocidad}</Text>
+            <View style={global.statsCont}>
+                <Text style={global.statsText}>Fuerza: <Text style={global.statsDato}>{fuerza}</Text></Text>
+                <Text style={global.statsText}>Velocidad: <Text style={global.statsDato}>{velocidad}</Text></Text>
             </View>
-            <View>
-                <Text>Habilidades</Text>
+            <View style={global.statsCont}>
+                <Text style={global.habilidadesTit}>Habilidades</Text>
                 <View>
                      {habilidades.map(function(habilidad,index){
-                return (<Text key={index}>{habilidad}</Text>)
+                return (<Text style={global.habilidadesText} key={index}>{habilidad}</Text>)
              })}
 
                 </View>
            
             </View>
+            <Pressable onPress={function(){eliminar(id)}}><Text>Eliminar</Text></Pressable>
         </View>
     )
 }
